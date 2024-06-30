b0350423712bc785c6494905deb2bb7ab72d9c82
DavidElierCampaChaparro <141787493+davidelierspecter@users.noreply.github.com>
2024-06-29 17:37:12 -0700

    asd
    asdasd

--- JuaysApp_Negocio/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"negocio","l":"ChatBO"},{"p":"negocio","l":"MensajeBO"},{"p":"negocio","l":"UsuarioBO"}];updateSearchResults();
