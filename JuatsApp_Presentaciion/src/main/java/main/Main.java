12198630f75d6099d78069119f1839f7bf749355
DavidElierCampaChaparro <141787493+davidelierspecter@users.noreply.github.com>
2024-06-30 19:51:30 -0700

    ahora se mira mas chilo
    le puse el flatlaf  en las dependencias y agreguue en el main el flatlaf light

--- JuatsApp_Presentaciion/src/main/java/main/Main.java
+++ JuatsApp_Presentaciion/src/main/java/main/Main.java
@@ -4,6 +4,9 @@
  */
 package main;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import presentacion.Login;
 
 /**
@@ -14,20 +17,9 @@ public class Main {
     public static void main(String[] args) {
         
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
         }
         
         Login login = new Login();
